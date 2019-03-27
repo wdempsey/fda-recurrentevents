88d4e1fdf2ea3212905d27006997bd87f41331a9
wdempsey <dempsey.walter@gmail.com>
2019-03-27 11:51:54 -0400

    Parallel code for each participant with a bash script iterating over participants

---
+++ data_visualization/sbatch_plots_loop.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+for PARTICIPANTID in {1..91}; do
+    #
+    echo "${PARTICIPANTID}"
+    export PARTICIPANTID
+    #
+    sbatch -o MCsim_${PARTICIPANTID}.stdout.txt \
+	   -e MCsim_${PARTICIPANTID}.stdout.txt \
+           --job-name=analysisof_${PARTICIPANTID} \
+	   plots_loop.sbatch
+    #
+    sleep 1 # pause to be kind to the scheduler
+done
+
+
+
---
+++ data_visualization/weight_calculator_per_participant.R
@@ -0,0 +1,69 @@
+#First read in the arguments listed at the command line
+args=(commandArgs(TRUE))
+
+##args is now a list of character vectors
+## First check to see if arguments are passed.
+## Then cycle through each element of the list and evaluate the expressions.
+if(length(args)==0){
+  print("No arguments supplied.")
+  ##supply default values
+  id = 1040
+}else{
+  id = as.numeric(args[[1]])
+}
+
+print(paste("Current participant is", id))
+source('./functions.R')
+
+setwd("/Users/walterdempsey/Harvard University/R21_Study - EDA")
+buttonpress = readRDS("../R21_Study - tags/button_presses.RDS")
+
+file_name = paste(id,"_EDA.rds", sep = "")
+if(file.exists(file_name)) {
+  eda = readRDS(paste(id,"_EDA.rds", sep = ""))
+} else{
+  print(paste("No EDA file with id", id))
+  break
+}
+
+id_bp = subset(buttonpress, ID == id)
+library(doParallel)
+library(doRNG)
+
+cl = Sys.getenv("SLURM_NTASKS_PER_NODE")
+cl
+
+registerDoParallel(cores = (cl))
+
+# Shows the number of Parallel Workers to be used
+getDoParWorkers()
+
+sequence = seq(-30,10, by = 0.25) # sequence along which we compute
+
+event_times_output = foreach(bp_obs=1:nrow(id_bp), .combine = 'cbind') %dopar% 
+  event_kernel_apply(bp_obs, eda, id_bp, current_bp_ts, sequence)
+
+event_numerator_output = matrix(event_times_output[1,], ncol = length(sequence), byrow = TRUE)
+event_denominator_output = matrix(event_times_output[2,], ncol = length(sequence), byrow = TRUE)
+
+#outcome = colSums(numerator_output, na.rm = TRUE)/colSums(denominator_output, na.rm = TRUE)
+# plot(sequence, outcome-mean(outcome) , type = "l", 
+#      ylab = "FSF EDA (mean-centered)", xlab = "Time (minutes) prior to event time",
+#      main = paste("Participant",id))
+
+num.iters = 5000
+nonevent_times_output = foreach(bp_obs=1:num.iters, .combine = 'cbind', .options.RNG=197311) %dopar% 
+  nonevent_kernel_apply(eda, id_bp, sequence)
+
+nonevent_numerator_output = matrix(nonevent_times_output[1,], ncol = length(sequence), byrow = TRUE)
+nonevent_denominator_output = matrix(nonevent_times_output[2,], ncol = length(sequence), byrow = TRUE)
+#outcome = colSums(nonevent_numerator_output,na.rm = TRUE)/colSums(nonevent_denominator_output, na.rm=TRUE)
+
+full_data = list("event_numerator_output"=event_numerator_output, 
+                 "event_denominator_output"=event_denominator_output,
+                 "nonevent_numerator_output"=nonevent_numerator_output,
+                 "nonevent_numerator_output"=nonevent_numerator_output)
+
+setwd("~/Documents/github/fda-recurrentevents/data_visualization/data")
+saveRDS(object = full_data, file = paste("kerneloutput_id_",id,".RDS", sep=""))
+
