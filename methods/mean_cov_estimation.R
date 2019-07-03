a823b4e7cffd6cc79c33defc0686a1ce0fd0f752
wdempsey <dempsey.walter@gmail.com>
2019-07-03 16:56:20 -0400

    Fixed how saving works

--- methods/mean_cov_estimation.R
+++ methods/mean_cov_estimation.R
@@ -15,15 +15,15 @@
 
 setwd("/Volumes/murphy_lab/users/wdempsey/data-for-fda/data/")
 event_complete = readRDS("./event_complete_2019-07-01.RDS")
-sequence = seq(-30,0,by =1/60)
-plot(sequence, event_complete[3,4:1804], type = "l")
+sequence = seq(-30,0,by =1/60); sensor_obs = 4:1804
+plot(sequence, event_complete[3,sensor_obs], type = "l")
 
 library(refund)
 full_obs = apply(X = event_complete, MARGIN = 1, FUN = function(x){!any(is.na(x))})
-greater_than_ten_percent_missing = apply(X = event_complete, MARGIN = 1, FUN = function(x){mean(!is.na(x[4:1804])) > 0.1})
-greater_than_thirty_percent_missing = apply(X = event_complete, MARGIN = 1, FUN = function(x){mean(!is.na(x[4:1804])) > 0.3})
-greater_than_fifty_percent_missing = apply(X = event_complete, MARGIN = 1, FUN = function(x){mean(!is.na(x[4:1804])) > 0.5})
-Y = event_complete[full_obs,4:1804]
+greater_than_ten_percent_missing = apply(X = event_complete, MARGIN = 1, FUN = function(x){mean(!is.na(x[sensor_obs])) > 0.1})
+greater_than_thirty_percent_missing = apply(X = event_complete, MARGIN = 1, FUN = function(x){mean(!is.na(x[sensor_obs])) > 0.3})
+greater_than_fifty_percent_missing = apply(X = event_complete, MARGIN = 1, FUN = function(x){mean(!is.na(x[sensor_obs])) > 0.5})
+Y = event_complete[full_obs,sensor_obs]
 x = event_complete[full_obs,2]
 z = sequence
 est <- fbps(Y,list(x=x,z=z))
@@ -60,35 +60,35 @@ plot(sequence, phi_vectors%*%coef[obs,]/inflation + est$Yhat[obs,], type = "l")
 lines(sequence, Y[obs,], col = "red", lwd = 2)
 
 ## SAVE THE MEAN, COEFFICIENT MATRIX, AND FIRST K EIGEN VECTORS
-saveRDS(object = est$Yhat, file = "event_means.RDS")
-saveRDS(object = coef/inflation, file = "event_coef_matrix.RDS")
-saveRDS(object = phi_vectors, file = "event_eigen_vectors.RDS")
-saveRDS(object = x, file = "event_complete_case_times.RDS")
+saveRDS(object = est$Yhat, file = paste("event_means_",today(),".RDS", sep=""))
+saveRDS(object = coef/inflation, file = paste("event_coef_matrix_",today(),".RDS", sep=""))
+saveRDS(object = phi_vectors, file = paste("event_eigen_vectors_",today(),".RDS", sep=""))
+saveRDS(object = x, file = paste("event_complete_case_times_",today(),".RDS", sep=""))
 
 ## CLEAR WORKSPACE
 rm(list=ls()) 
-setwd("/Volumes/murphy_lab/users/wdempsey/fda-recurrent-events/methods/")
-nonevent_complete = readRDS("./nonevent_complete.RDS")
-sequence = seq(-30,0,by =1/60)
-plot(sequence, nonevent_complete[3,3:1803], type = "l")
+setwd("/Volumes/murphy_lab/users/wdempsey/data-for-fda/data/")
+nonevent_complete = readRDS("./nonevent_complete_2019-07-01.RDS")
+sequence = seq(-30,0,by =1/60); sensor_obs = 4:1804
+plot(sequence, nonevent_complete[3,sensor_obs], type = "l")
 
 library(refund)
 full_obs = apply(X = nonevent_complete, MARGIN = 1, FUN = function(x){!any(is.na(x))})
-Y = nonevent_complete[full_obs,3:1803]
+Y = nonevent_complete[full_obs,sensor_obs]
 x = nonevent_complete[full_obs,2]
 z = sequence
 rm("nonevent_complete") # Remove the full data to free up memory
 
 est <- fbps(Y,list(x=x,z=z))
-obs = 100
+obs = 30
 plot(sequence,est$Yhat[obs,]- mean(est$Yhat[obs,]))
 lines(sequence, Y[obs,]-mean(Y[obs,]))
 
 ## COMPUTE ESTIMATED MARGINAL COVARIANCE 
 inflation = 100
 residual = inflation*(Y-est$Yhat)
-saveRDS(object = est$Yhat, file = "nonevent_means.RDS")
-rm(c("Y", "est"))
+saveRDS(object = est$Yhat, file = paste("nonevent_means_",today(),".RDS", sep=""))
+rm("Y"); rm("est")
 ## TAKE PAIRS OF ROWS and CALCULATE THE MSE
 Sigma = matrix(nrow = ncol(residual), ncol = ncol(residual))
 for (i in 1:ncol(residual)) {
@@ -114,6 +114,6 @@ plot(sequence, phi_vectors%*%coef[obs,]/inflation + est$Yhat[obs,], type = "l")
 lines(sequence, Y[obs,], col = "red", lwd = 2)
 
 ## SAVE THE MEAN, COEFFICIENT MATRIX, AND FIRST K EIGEN VECTORS
-saveRDS(object = coef/inflation, file = "nonevent_coef_matrix.RDS")
-saveRDS(object = phi_vectors, file = "nonevent_eigen_vectors.RDS")
-saveRDS(object = x, file = "nonevent_complete_case_times.RDS")
+saveRDS(object = coef/inflation, file = paste("nonevent_coef_matrix_",today(),".RDS", sep=""))
+saveRDS(object = phi_vectors, file = paste("nonevent_eigen_vectors_",today(),".RDS", sep=""))
+saveRDS(object = x, file = paste("nonevent_complete_case_times_",today(),".RDS", sep=""))
