63f25dcae9a8be116d11f90f16c17224e6bc6cab
Walter Dempsey <wdem@gl-login1.arc-ts.umich.edu>
2020-06-27 17:27:44 -0400

    fixed a few lines to submit to gl

--- simulations/simsloop.bash
+++ simulations/simsloop.bash
@@ -3,10 +3,7 @@ for ITER in {1..2}; do
     #
     echo "${ITER}"
     #
-    sbatch -o MCsim_${ITER}.stdout.txt \
-	   -e MCsim_${ITER}.stdout.txt \
-           --job-name=my_analysis_${ITER} \
-	   simsloop.sbatch
+    sbatch --job-name=my_analysis_${ITER} simsloop.sbatch
     #
     sleep 10 # pause to be kind to the scheduler
 done
