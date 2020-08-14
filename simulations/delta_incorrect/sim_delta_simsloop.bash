fa461e351e3aab9aa7be398a4ebdc835f0fca04f
Walter Dempsey <dempsey.walter@gmail.com>
2020-08-14 11:17:01 -0400

    bash and sbatch scripts

--- simulations/delta_incorrect/sim_delta_simsloop.bash
+++ simulations/delta_incorrect/sim_delta_simsloop.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-for ITER in {1..2}; do
+for ITER in {1..1000}; do
     #
     echo "${ITER}"
     #
     sbatch --job-name=my_analysis_${ITER} simsloop.sbatch
     #
-    sleep 10 # pause to be kind to the scheduler
+    sleep 5 # pause to be kind to the scheduler
 done
