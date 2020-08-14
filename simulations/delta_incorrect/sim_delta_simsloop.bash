e33906c65f56012bced1c79e57189922d580f483
Walter Dempsey <wdem@gl-login2.arc-ts.umich.edu>
2020-08-14 11:30:40 -0400

    fixed bug in bash file

--- simulations/delta_incorrect/sim_delta_simsloop.bash
+++ simulations/delta_incorrect/sim_delta_simsloop.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-for ITER in {1..1000}; do
+for ITER in {1..2}; do
     #
     echo "${ITER}"
     #
-    sbatch --job-name=my_analysis_${ITER} simsloop.sbatch
+    sbatch --job-name=my_analysis_${ITER} sim_delta_simsloop.sbatch
     #
     sleep 5 # pause to be kind to the scheduler
 done
