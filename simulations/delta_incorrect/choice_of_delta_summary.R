2ed7edbbc95f58625e9e31af6b13729c0fd7aebc
wdempsey <dempsey.walter@gmail.com>
2023-03-30 12:12:25 -0400

    fix the write-up

--- simulations/delta_incorrect/choice_of_delta_summary.R
+++ simulations/delta_incorrect/choice_of_delta_summary.R
@@ -1,38 +1,38 @@
-make_table <- function(setting) {
-  
-  test36 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_36.csv", sep = ""))
-  test40 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_40.csv", sep = ""))
-  test44 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_44.csv", sep = ""))
-  test48 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_48.csv", sep = ""))
-  test52 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_52.csv", sep = ""))
-  
-  overlapping_ids = Reduce(intersect, list(test36$ids, test40$ids, test44$ids, test48$ids, test52$ids))
-  overlapping_ids = overlapping_ids[order(overlapping_ids)]
-  whichmin = function(x) {which(x == min(x))}
-  result = rep(0,0)
-  for(id in overlapping_ids) {
+  make_table <- function(setting) {
     
-    temp = cbind(subset(test36, ids == id)[,4],
-                 subset(test40, ids == id)[,4],
-                 subset(test44, ids == id)[,4],
-                 subset(test48, ids == id)[,4],
-                 subset(test52, ids == id)[,4])
-    result_id = cbind(id, apply(temp, 1, whichmin), subset(test36, ids == id)$rates)
-    result = rbind(result, result_id)
+    test36 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_36.csv", sep = ""))
+    test40 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_40.csv", sep = ""))
+    test44 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_44.csv", sep = ""))
+    test48 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_48.csv", sep = ""))
+    test52 = read.csv(paste("./output_csv/", setting, "_simdelta_allresults_wl_52.csv", sep = ""))
     
+    overlapping_ids = Reduce(intersect, list(test36$ids, test40$ids, test44$ids, test48$ids, test52$ids))
+    overlapping_ids = overlapping_ids[order(overlapping_ids)]
+    whichmin = function(x) {which(x == min(x))}
+    result = rep(0,0)
+    for(id in overlapping_ids) {
+      
+      temp = cbind(subset(test36, ids == id)[,4],
+                   subset(test40, ids == id)[,4],
+                   subset(test44, ids == id)[,4],
+                   subset(test48, ids == id)[,4],
+                   subset(test52, ids == id)[,4])
+      result_id = cbind(id, apply(temp, 1, whichmin), subset(test36, ids == id)$rates)
+      result = rbind(result, result_id)
+      
+    }
+    
+    result = data.frame(result)
+    names(result) = c("id", "choice", "rate")
+    dataset = rbind(table(factor(subset(result, rate == 1.0)[,2], levels = 1:5)),
+                    table(factor(subset(result, rate == 0.5)[,2], levels = 1:5)),
+                    table(factor(subset(result, rate == 0.25)[,2], levels = 1:5)),
+                    table(factor(subset(result, rate == 0.125)[,2], levels = 1:5)))
+    normalized_dataset = diag(1/rowSums(dataset))%*%dataset
+    
+    return(t(cbind(unique(result$rate), round(normalized_dataset,2))))
   }
   
-  result = data.frame(result)
-  names(result) = c("id", "choice", "rate")
-  dataset = rbind(table(factor(subset(result, rate == 1.0)[,2], levels = 1:5)),
-                  table(factor(subset(result, rate == 0.5)[,2], levels = 1:5)),
-                  table(factor(subset(result, rate == 0.25)[,2], levels = 1:5)),
-                  table(factor(subset(result, rate == 0.125)[,2], levels = 1:5)))
-  normalized_dataset = diag(1/rowSums(dataset))%*%dataset
+  make_table(setting = "exponential")
   
-  return(t(cbind(unique(result$rate), round(normalized_dataset,2))))
-}
-
-make_table(setting = "exponential")
-
-make_table(setting = "sine")
+  make_table(setting = "sine")
