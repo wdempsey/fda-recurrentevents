ea4b5e28b2c8cc232a30ac79fc83bbca5c48eebd
Walter Dempsey <dempsey.walter@gmail.com>
2023-04-06 15:39:51 -0400

    Update mean_cov_estimation.R

--- methods/mean_cov_estimation.R
+++ methods/mean_cov_estimation.R
@@ -90,9 +90,10 @@ rm(list=ls())
 setwd("Z:/SI_data/")
 ## Linux
 set_of_types = c("eda", "acc")
+Delta = 30
 for (type in set_of_types){
   nonevent_complete = readRDS(paste(type, "_nonevent_complete_HLP_2021-11-20.RDS", sep=""))
-  sequence = seq(0,-30,length.out = ncol(nonevent_complete) - 3); sensor_obs = 4:ncol(nonevent_complete)
+  sequence = seq(0,-Delta,length.out = ncol(nonevent_complete) - 3); sensor_obs = 4:ncol(nonevent_complete)
   # plot(sequence, nonevent_complete[4,sensor_obs])
   
   library(refund); library(lubridate)
@@ -101,7 +102,6 @@ for (type in set_of_types){
   Y = nonevent_complete[full_obs,sensor_obs]
   Y = as.matrix(Y, nrow = nrow(Y), ncol = ncol(Y))
   mu = rowMeans(Y)
-  quad_Y = sweep(Y,1,mu)^2
   x = as.numeric(nonevent_complete[full_obs,3])
   z = sequence
   id = nonevent_complete[full_obs,1]
@@ -110,15 +110,12 @@ for (type in set_of_types){
   rm("nonevent_complete") # Remove the full data to free up memory
   
   est <- fbps(Y,list(x=x,z=z))
-  quad_est <- fbps(quad_Y,list(x=x,z=z))
   
   ## COMPUTE ESTIMATED MARGINAL COVARIANCE 
   inflation = 100
   residual = inflation*(Y-est$Yhat)
-  quad_residual = inflation*(quad_Y-quad_est$Yhat)
-  saveRDS(object = est$Yhat, file = paste(type, "_lin_nonevent_means_",today(),".RDS", sep=""))
-  saveRDS(object = quad_est$Yhat, file = paste(type, "_quad_nonevent_means_",today(),".RDS", sep=""))
-  rm("Y"); rm("est"); rm("quad_Y"); rm("quad_est")
+  saveRDS(object = est$Yhat, file = paste(type, "_lin_nonevent_means_Delta_",Delta,"_",today(),".RDS", sep=""))
+  rm("Y"); rm("est")
   ## TAKE PAIRS OF ROWS and CALCULATE THE MSE
   print(paste("Made it to nonevent, linear Sigma calc for", type))
   Sigma = matrix(nrow = ncol(residual), ncol = ncol(residual))
@@ -129,8 +126,8 @@ for (type in set_of_types){
   }
   Sigma_est <- fbps(Sigma)
   final_Sigma = (Sigma_est$Yhat + t(Sigma_est$Yhat))/2
-  saveRDS(object = final_Sigma, file = paste(type, "_nonevent_Sigma_",today(),".RDS", sep=""))
-  eig_Sigma_est = eigen(final_Sigma$Yhat)
+  saveRDS(object = final_Sigma, file = paste(type, "_nonevent_Sigma_Delta_",Delta,"_",today(),".RDS", sep=""))
+  eig_Sigma_est = eigen(final_Sigma)
   eig_vectors = eig_Sigma_est$vectors
   eig_values = eig_Sigma_est$values
   
@@ -143,37 +140,10 @@ for (type in set_of_types){
   
   
   ## SAVE THE MEAN, COEFFICIENT MATRIX, AND FIRST K EIGEN VECTORS
-  saveRDS(object = coef/inflation, file = paste(type, "_lin_nonevent_coef_matrix_",today(),".RDS", sep=""))
-  saveRDS(object = phi_vectors, file = paste(type, "_lin_nonevent_eigen_vectors_",today(),".RDS", sep=""))
-  saveRDS(object = x, file = paste(type, "_lin_nonevent_complete_case_timesincebaseline_",today(),".RDS", sep=""))
-  saveRDS(object = timestamp, file = paste(type, "_lin_nonevent_complete_case_timestamp_",today(),".RDS", sep=""))
+  saveRDS(object = coef/inflation, file = paste(type, "_lin_nonevent_coef_matrix_Delta_",Delta,"_",today(),".RDS", sep=""))
+  saveRDS(object = phi_vectors, file = paste(type, "_lin_nonevent_eigen_vectors_Delta_",Delta,"_",today(),".RDS", sep=""))
+  saveRDS(object = x, file = paste(type, "_lin_nonevent_complete_case_timesincebaseline_Delta_",Delta,"_",today(),".RDS", sep=""))
+  saveRDS(object = timestamp, file = paste(type, "_lin_nonevent_complete_case_timestamp_Delta_",Delta,"_",today(),".RDS", sep=""))
   saveRDS(object = id, 
-          file = paste(type, "_nonevent_complete_case_ids_",today(),".RDS", sep=""))
-  
-  ## TAKE PAIRS OF ROWS and CALCULATE THE MSE
-  print(paste("Made it to nonevent, quadratic Sigma calc for", type))
-  Sigma = matrix(nrow = ncol(quad_residual), ncol = ncol(quad_residual))
-  for (i in 1:ncol(quad_residual)) {
-    for (j in 1:ncol(quad_residual)) {
-      Sigma[i,j] = sum(quad_residual[,i]*quad_residual[,j])/nrow(quad_residual)
-    }
-  }
-  
-  Sigma_est <- fbps(Sigma)
-  Sigma_est$Yhat[1:10,1:10]
-  eig_Sigma_est = eigen(Sigma_est$Yhat)
-  
-  eig_vectors = eig_Sigma_est$vectors
-  eig_values = eig_Sigma_est$values
-  
-  eig_values[eig_values<0] = 0
-  
-  K = 35
-  print(paste("Variance explained: ", (cumsum(eig_values)/sum(eig_values))[K]))
-  phi_vectors = eig_vectors[,1:K]
-  coef = residual%*%phi_vectors
-
-  ## SAVE THE MEAN, COEFFICIENT MATRIX, AND FIRST K EIGEN VECTORS
-  saveRDS(object = coef/inflation, file = paste(type, "_quad_nonevent_coef_matrix_",today(),".RDS", sep=""))
-  saveRDS(object = phi_vectors, file = paste(type, "_quad_nonevent_eigen_vectors_",today(),".RDS", sep=""))
+          file = paste(type, "_nonevent_complete_case_ids_Delta_",Delta,"_",today(),".RDS", sep=""))
 }
