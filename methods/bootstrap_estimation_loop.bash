da2907b868ecb39afc641388d6d8def1e1f0a2cc
Walter Dempsey <wdem@umich.edu>
2023-05-08 10:25:12 -0400

    hoping it works

--- methods/bootstrap_estimation_loop.bash
+++ methods/bootstrap_estimation_loop.bash
@@ -2,11 +2,11 @@
 
 for $BS in {1..2}
 do
-	for $MI in {1..2}
-	do
-		for $DELTA in 5 15 30
-		do
-			sbatch
-		done
-	done
+  for $MI in {1..2}
+  do
+    for $DELTA in 5 15 30
+    do
+      sbatch bootstrap_estimation_loop.sbatch
+    done
+  done
 done
