c9b03ef104d367b51520f275478ceabee29bde68
Walter Dempsey <wdem@umich.edu>
2023-05-08 21:43:46 -0400

    here is final update from online

--- methods/bootstrap_estimation_loop.bash
+++ methods/bootstrap_estimation_loop.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-for BS in 1
+for BS in {1..200}
 do
-    for MI in 1
+    for MI in {1..2}
     do
-	for DELTA in 15
+	for DELTA in 5 15 30
 	do
             echo "Bootstrap number $BS, MI number $MI, current DELTA is $DELTA"
 	    export BS=$BS,MI=$MI,DELTA=$DELTA
 	    sbatch --export=BS=$BS,MI=$MI,DELTA=$DELTA bootstrap_estimation_loop.sbatch
-	    sleep 5
+	    sleep 1
 	done
     done
 done
